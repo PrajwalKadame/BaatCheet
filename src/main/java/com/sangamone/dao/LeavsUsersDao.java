b73fa5f1a55659eaba6783cdd7429e8163333927
PrajwalKadame <prajwalkadame.sangamone@gmail.com>
2023-10-16 14:50:02 +0530

    LEAVS Project

---
+++ src/main/java/com/sangamone/dao/LeavsUsersDao.java
@@ -0,0 +1,14 @@
+package com.sangamone.dao;
+
+import java.util.List;
+
+import com.sangamone.model.LeavsCompanyUser;
+import com.sangamone.model.LeavsUsers;
+
+public interface LeavsUsersDao {
+
+	String addLeavsUser(LeavsCompanyUser leavsCompanyUser);
+
+	List<LeavsUsers> viewLeavsUsers();
+
+}
---
+++ src/main/java/com/sangamone/repository/LeavsUsersRepo.java
@@ -0,0 +1,9 @@
+package com.sangamone.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sangamone.model.LeavsUsers;
+
+public interface LeavsUsersRepo extends JpaRepository<LeavsUsers, Integer>{
+
+}
